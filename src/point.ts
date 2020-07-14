1e0d23067e2adfb6e4033cbfa708334afd4744ff
Dmitriy Bespalov <dbespalov.rosko@gmail.com>
2020-07-14 22:15:45 +0300

    feat(src): add point class

---
+++ src/point.ts
@@ -0,0 +1,19 @@
+/**
+ * Class Point
+ */
+class Point {
+  public x: number;
+  public y: number;
+
+  /**
+   * Constructor
+   * @param {int} x
+   * @param {int} y
+   */
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export {Point};
